ca9782d5063f02e423c1c98d51126d9db534fe7c
Baris Ceviz <baris@ceviz.dev>
2022-12-04 20:04:31 +0100

    feat: add token support to check balance

--- api/src/app/controllers/balance.controller.ts
+++ api/src/app/controllers/balance.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { Token } from '../decorators/token.decorator';
 import { BalanceService } from '../services/balance.service';
 
 @Controller('v1/balance')
@@ -7,10 +8,12 @@ export class BalanceController {
 
   @Get('check')
   async checkBalance(
+    @Token() token: string,
     @Query('number') number: string,
   ): Promise<{ value: string }> {
     return await this.balanceService.getBalance(
       this.balanceService.formatCardNumber(number),
+      token,
     );
   }
 }
--- api/src/app/services/balance.service.ts
+++ api/src/app/services/balance.service.ts
@@ -2,20 +2,26 @@ import type { Browser } from 'puppeteer';
 import { Injectable } from '@nestjs/common';
 import { InjectBrowser } from 'nest-puppeteer';
 import { clearBrowser } from 'src/utils/clear-browser';
+import { setTokenToBrowser } from '../guards/auth-browser';
 
 @Injectable()
 export class BalanceService {
   constructor(@InjectBrowser() private readonly browser: Browser) {}
 
-  async getBalance(number: string): Promise<{ value: string }> {
+  async getBalance(number: string, token: string): Promise<{ value: string }> {
     const context = await this.browser.createIncognitoBrowserContext();
 
     const page = await context.newPage();
     await clearBrowser(page);
 
+    if (token) {
+      await setTokenToBrowser(token, page);
+    }
+
     await page.goto('https://www.ov-chipkaart.nl/home.htm#/', {
       waitUntil: 'networkidle0',
     });
+
     const inputEl = await page.waitForSelector('#mediumInput');
     await inputEl.type(number);
 
@@ -36,7 +42,9 @@ export class BalanceService {
     );
 
     const value = await priceEl.evaluate((el) => el.textContent);
-
+    await page.screenshot({
+      path: 'screenshots/balance-1.png',
+    });
     await context.close();
 
     return { value };
