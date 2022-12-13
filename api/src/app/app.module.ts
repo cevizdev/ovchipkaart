e4316999992980bde54062bba5458cbac342b2c1
Baris Ceviz <baris@ceviz.dev>
2022-12-13 17:04:01 +0100

    feat: change dockerfile base image to alpine

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PlaywrightModule } from 'nestjs-playwright';
     PlaywrightModule.forRoot({
       headless: true,
       isGlobal: true,
+      executablePath: process.env.CHROME_BIN,
     }),
     PlaywrightModule.forFeature(),
   ],
